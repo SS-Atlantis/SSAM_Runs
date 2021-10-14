55cca849e127eea7469c19e183730e31519ff1df
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2021-10-14 08:19:44 -0700

    half light absorption by detritus, k_DL=0.0019

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/14yHydro_reducedDet
+atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/14yHydro_halfDetLightAbs
 
