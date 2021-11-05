a5a474145180265112cacb21d2c411e96ceed2af
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2021-11-05 12:31:40 -0700

    updated primary producer

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/14yHydro_halfDetLightAbs
+atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/20211105_bio_update
 
