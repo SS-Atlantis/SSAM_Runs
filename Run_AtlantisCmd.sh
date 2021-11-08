32fa59e47d12033b49d385d18b3c39cbe2f197cb
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2021-11-07 19:00:45 -0800

    100y

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/20211105_bio_update
+atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/20211107_100y
 
