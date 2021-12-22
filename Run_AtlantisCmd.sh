f9591d5740ae10d8f7a65c3b1260d7cb7a13d765
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2021-12-22 11:01:55 -0800

    update contam code and test

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/test_contaminants
+atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/contam_test2
 
