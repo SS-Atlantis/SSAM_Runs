90006a7317db4d6cb182678de2ff48a4e6776d8c
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2022-04-12 11:42:30 -0700

    run: oil injected in yr70

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_TurnPoint_20200119_day2release
+atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_TurnPoint_20200119_75y
