50ab113c89e976f1c9433f87862f863ab95b5b63
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2022-05-06 13:04:05 -0700

    test for timing of oil injection on new branch code

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_TurnPoint_20200119_box60
+atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_TurnPoint_1960_test
