7a775ca608d8d78439049799594d1fca618aebc1
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2022-05-16 08:01:38 -0700

    separate annual contam forcing files

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_TurnPoint_1960_test
+atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_turnPoint_04SS_75y
