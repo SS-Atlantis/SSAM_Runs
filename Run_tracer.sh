050a0763862643b217835b4bf8de34273f50a1b8
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2022-09-01 08:12:17 -0700

    run tracer dispersion test in Atlantis with no bio uptake

--- Run_tracer.sh
+++ Run_tracer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_tracer.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Control_w0tracer
+atlantis run ./atlantis_tracer2.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_Tracer_10d
