1e7f60e6e74e8261c4958a7e2cd5d51636c3347b
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2022-11-21 15:41:43 -0800

    test multi-day contam injection without biology

--- Run_tracer.sh
+++ Run_tracer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_tracer2.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_Tracer_10d
+atlantis run ./atlantis_tracer2.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_tracer_12d_reset
