2fc9c79229a90c1fb485e9d564de85ed709b8058
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2022-11-21 15:45:34 -0800

    multi-day contam injection with flag_contam_sanity_check = 0

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_TurnPoint_04SS_75y
+atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_2020spill_35yrun_02
