f61fe319b53aae1212471bb34781d54f6ae3b48b
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2022-11-23 13:03:48 -0800

    flag_contam_sanity_check off, lower contam uptake_rate values

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_2020spill_35yrun_02
+atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_2020spill_35yrun_03
