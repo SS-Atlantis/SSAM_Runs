1aab5c4759366dbcc6ceae94126d06b68847124f
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2022-12-02 09:13:26 -0800

    test range of value for uptake_rate in sigmoid eqn

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_2020spill_35yrun_06
+atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_1993spill_sigmoid_uptake_08
