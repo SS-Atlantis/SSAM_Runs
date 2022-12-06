68658c3c714233570da29c755910b7c744b1d6cc
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2022-12-06 13:23:06 -0800

    test high zoo uptake rate

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_1993spill_linear_uptake_05
+atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/5b_1993spill_linear_uptake_07
