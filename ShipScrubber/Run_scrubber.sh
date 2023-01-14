804e6c54d01456315826e3d174853d9da5b571d3
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2023-01-14 13:46:33 -0800

    check mortality and direct effects at discharge time

--- ShipScrubber/Run_scrubber.sh
+++ ShipScrubber/Run_scrubber.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_scrubber.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Scrubber_discharge_2019-2029_02
+atlantis run ./atlantis_scrubber.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Scrubber_discharge_2019_highres_mort
