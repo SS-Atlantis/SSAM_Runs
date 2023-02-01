a1fbf173380bef3b0f675458d2714eede4f9d112
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2023-02-01 06:45:23 -0800

    test calibration: 100y controls

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/TurnPoint_2020_control
+atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/TurnPoint_2020_control_100y
--- ShipScrubber/Run_scrubber.sh
+++ ShipScrubber/Run_scrubber.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_scrubber.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Scrubber_discharge_2019_500
+atlantis run ./atlantis_scrubber.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Scrubber_discharge_control_100y
