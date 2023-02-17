f3c383560366146092f3ef11e4744784a6332f60
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2023-02-17 11:44:23 -0800

    updated control, model start 2007

--- LivingLabs/Run_livinglabs.sh
+++ LivingLabs/Run_livinglabs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_livinglabs.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/LivingLabs/climate_SSP585_1
+atlantis run ./atlantis_livinglabs.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/LivingLabs/control_01
