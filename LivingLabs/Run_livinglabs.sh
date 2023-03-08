efbff7b22c34e6371e98bf5cdda2c71eec338465
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2023-03-08 11:25:25 -0800

    adjusted model temp and heatwave return

--- LivingLabs/Run_livinglabs.sh
+++ LivingLabs/Run_livinglabs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_livinglabs.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/LivingLabs/heatwave6_fullrun
+atlantis run ./atlantis_livinglabs.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/LivingLabs/climate_SSP585_adjustedT
