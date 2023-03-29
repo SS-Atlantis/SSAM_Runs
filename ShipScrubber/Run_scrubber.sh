93b199044904afc76cc86e63b8e5ec5f44a492d1
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2023-03-28 19:03:10 -0700

    correct run file for normal resolution simulation

--- ShipScrubber/Run_scrubber.sh
+++ ShipScrubber/Run_scrubber.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_scrubber_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Scrubber/Scrubber_discharge_2019_highres
+atlantis run ./atlantis_scrubber.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Scrubber/Scrubber_discharge_100y_2024
