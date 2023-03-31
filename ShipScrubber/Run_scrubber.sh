aa57d8a08b1f454c9bdb7c99295a51d11ab9b615
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2023-03-31 15:18:41 -0700

    correct discharge start dates

--- ShipScrubber/Run_scrubber.sh
+++ ShipScrubber/Run_scrubber.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_scrubber.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Scrubber/Scrubber_discharge_100y_2024
+atlantis run ./atlantis_scrubber.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Scrubber/Scrubber_discharge_100y_2029
