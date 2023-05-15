a5fa8e00cece88091fb915ecedb8b71de1dc88ff
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2023-05-15 11:17:51 -0700

    Scenario_4a_2020-07-12

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/TurnPoint_2020_control_100y
+atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Scenario_4a_2020-07-12
