ef190fee796375e1c93d4b8e40424fb6eeb18b13
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2023-05-29 15:11:42 -0700

    Scenario 4a 2020-07-12

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Scenario_4a_2020-07-20
+atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Scenario_4a_2020-07-12
