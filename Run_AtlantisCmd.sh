f6c634cbee49643f9647e6c7940a81261bb5ba26
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2023-07-09 22:24:15 -0700

    spring 5b test runs

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_spring.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Scenario_5b_2019-04-06
+atlantis run ./atlantis_spring.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Scenario_5b_2020-04-11
