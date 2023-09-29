39a430129261705204b4ba4d10cc678a0fc2890f
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2023-09-28 17:17:40 -0700

    update parameters and test contam accounting in and out of water column

--- Run_test.sh
+++ Run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1_nozeroes.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Test_Scenario_5b_11
+atlantis run ./atlantis_year1_nozeroes.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Test_Scenario_5b_12
