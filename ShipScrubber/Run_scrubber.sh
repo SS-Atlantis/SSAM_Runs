0752e69af1b4f17b4d1abfc1f73786366fb524e8
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2023-12-18 16:00:43 -0800

    sort files for the Ship Scrubber project

--- ShipScrubber/Run_scrubber.sh
+++ ShipScrubber/Run_scrubber.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_scrubber.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Scrubber/Scrubber_discharge_100y_2029
+atlantis run ./atlantis_scrubber_control.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Sensitivity/butterflyeffect_PLx11
