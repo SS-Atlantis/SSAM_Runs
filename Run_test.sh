99e2663a8ebfeffb206ee2427a665a29f90c3268
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-01-05 13:41:34 -0800

    run with v6699 test 16 but with flag_dissolved_pollutants set to 0

--- Run_test.sh
+++ Run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Test_Scenario_5b_v6699_adjusted
+atlantis run ./atlantis_year1.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Test_Scenario_5b_v6699_flag_dissolved_pollutants0
