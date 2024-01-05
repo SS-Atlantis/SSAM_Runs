97b3ebea6457b1416dce51856690f13df8d2e799
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-01-05 13:47:59 -0800

    repeat 17 but with flag_dissolved_pollutants turned off

--- Run_test.sh
+++ Run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Test_Scenario_5b_v6699_17
+atlantis run ./atlantis_year1.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Test_Scenario_5b_v6699_17_flag_dissolved_pollutants0
