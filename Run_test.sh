d252d805c546574c2599998de67ac846acfbd4ac
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-01-05 14:29:14 -0800

    reduce Contam_uptake_rates

--- Run_test.sh
+++ Run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Test_Scenario_5b_v6699_17_flag_dissolved_pollutants0
+atlantis run ./atlantis_year1.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Test_Scenario_5b_v6699_17_reduced_uptake
