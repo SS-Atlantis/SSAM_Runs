2faed7bb83da8d471cd6de4831badb9a7bdc5c5a
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-01-19 08:36:32 -0800

    runs to solve contam build-up in biopools

--- Run_highres.sh
+++ Run_highres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Test_Scenario_5b_v6699_17_highres
+atlantis run ./atlantis_year1_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Test_Scenario_5b_v6699_18F_highres_90d
--- Run_test.sh
+++ Run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Test_Scenario_5b_v6699_17_reduced_uptake
+atlantis run ./atlantis_year1.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Test_Scenario_5b_v6699_18F_HR1yr
