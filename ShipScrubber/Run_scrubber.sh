29f2a0a79db36707de4e13086701b45c78ab14b1
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-01-24 20:42:51 -0800

    update for v6699

--- ShipScrubber/Run_scrubber.sh
+++ ShipScrubber/Run_scrubber.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_scrubber_control.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Sensitivity/butterflyeffect_PLx11
+atlantis run ./atlantis_scrubber.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Scrubber/v6699_discharge_100y_2019
