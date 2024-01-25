4b89bf7526f763593f1ef58fa92683b7a17971bc
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-01-24 20:53:17 -0800

    100y control run

--- ShipScrubber/Run_scrubber.sh
+++ ShipScrubber/Run_scrubber.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_scrubber.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Scrubber/v6699_discharge_100y_2019
+atlantis run ./atlantis_scrubber.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Scrubber/v6699_discharge_100y_control
