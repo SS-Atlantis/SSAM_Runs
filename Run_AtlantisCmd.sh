91e3cfe9f7016f54606ec85d01f92ef21a6cfd3a
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-03-28 08:39:55 -0700

    Rerun of 5b spills with v6699 testing 1K, 5K, and 10K particles

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_summer.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Scenario_5b_2019-07-03_1000_new
+atlantis run ./atlantis_summer.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/v6699_Scenario_5b_2019-07-03_10000
