250844e6675d514e3a0945149e5fe935ae6520bc
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-04-12 10:01:39 -0700

    test v6699 sigmoid uptake m = 0.67

--- Run_highres.sh
+++ Run_highres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/5b_v6699_sigmoid_m98
+atlantis run ./atlantis_year1_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/5b_v6699_sigmoid_m67
