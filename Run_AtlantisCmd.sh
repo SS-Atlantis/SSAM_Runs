0af27ec46699a2e70bca672f986a177a3f870dd2
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-05-23 14:38:21 -0700

    new calibration + oil, new linear uptake, 50y, summer discharge, 5000p

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_summer.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/v6699_Scenario_5b_2019-07-03_5000_uptakeoff
+atlantis run ./atlantis_summer.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/v6699_Scenario_5b_2019-07-03_5000_20240523
