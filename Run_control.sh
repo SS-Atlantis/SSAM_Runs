7ccb7b417984ab566a9b2bd01a76a12dda2f3b37
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-05-23 14:21:11 -0700

    checking updated calibration on 00SS_biology

--- Run_control.sh
+++ Run_control.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Control/v6708_50y_20240411
+atlantis run ./atlantis.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Control/v6699_50y_20240523
