6597d6b300a957bc88d80ad938db28e5da219807
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-06-10 10:27:54 -0700

    reduce contam uptake to <1% of release mass

--- Run_highres.sh
+++ Run_highres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/5b_v6699_mod_linear_newrates
+atlantis run ./atlantis_year1_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/5b_v6699_mod_linear_newratesB
