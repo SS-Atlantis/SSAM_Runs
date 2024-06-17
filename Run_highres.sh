8f662e5eaaa468934760b5c6deebb3d9b1f268a2
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-06-17 09:00:21 -0700

    1000mg high res spillrrun for equations paper - lower trophic levels

--- Run_highres.sh
+++ Run_highres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/5b_v6699_mod_linear_newratesB
+atlantis run ./atlantis_year1_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/7a_v6699_1000mg_single_highres
