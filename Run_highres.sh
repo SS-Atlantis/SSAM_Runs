a4c8e6820e87d10f3a6eb86700059d206a1df069
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-06-17 10:55:31 -0700

    updated v6699 control run with 1-day output for 90 days at simulation start

--- Run_highres.sh
+++ Run_highres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/7a_v6699_1000mg_single_highres
+atlantis run ./atlantis_year1_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/7a_v6699_1000mg_single_highres_control
