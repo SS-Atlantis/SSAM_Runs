fa4dd5dac09eb3cf430f2c2ce3e5e42be9a1125c
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-06-18 11:26:45 -0700

    increase injection amount to 1000kg

--- Run_highres.sh
+++ Run_highres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/7a_v6699_1000mg_single_highres_control
+atlantis run ./atlantis_year1_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/7a_v6699_1000kg_single_highres
