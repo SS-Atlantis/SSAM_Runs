8049d6d964bc0092fed5ecd936661ee6f07c5a19
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-06-20 13:32:47 -0700

    1 day output. Others were 0.5 day

--- Run_highres.sh
+++ Run_highres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_d300_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/7a_v6699_1000kg_300d_highres
+atlantis run ./atlantis_d300_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/7a_v6699_1000kg_360d_highres_daily
