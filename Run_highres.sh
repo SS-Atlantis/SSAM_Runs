4b15a476ba08fd5f695c8fe71f8345c9b8334724
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-07-11 21:01:10 -0700

    modified p_ageup returns worse results. changed back

--- Run_highres.sh
+++ Run_highres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/age_contaminants_modified_p_ageup
+atlantis run ./atlantis_year1_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/age_contaminants_store3
