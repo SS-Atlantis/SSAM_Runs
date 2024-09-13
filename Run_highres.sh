af8b3bf949d2e4032a134af476716d5545a3abb1
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-09-13 08:50:10 -0700

    lower trophic level run for equations paper with older bio file

--- Run_highres.sh
+++ Run_highres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/age_contaminants_store3
+atlantis run ./atlantis_year1_highres_EqnsPaper.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/7a-EquationsPaper-1
