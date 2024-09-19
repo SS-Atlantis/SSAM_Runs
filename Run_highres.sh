cfc86afc36ca7963f99cc6c9cc4d3d26f9118616
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-09-18 18:56:02 -0700

    fprints of contamSpMort for Naph

--- Run_highres.sh
+++ Run_highres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1_highres_EqnsPaper.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/7a-EquationsPaper-SpMort-Benzo
+atlantis run ./atlantis_year1_highres_EqnsPaper.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/7a-EquationsPaper-SpMort-Naph
