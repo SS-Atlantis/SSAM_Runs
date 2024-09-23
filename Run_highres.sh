aa4615b605bfc0e19e918ce378cc217e8e599848
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-09-23 13:18:50 -0700

    add biology.prm file for EqnsPaper. update run for biological effects with temperature-dependence turned off

--- Run_highres.sh
+++ Run_highres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1_highres_EqnsPaper.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/7a-EquationsPaper-Cgrowth-Naph
+atlantis run ./atlantis_year1_highres_EqnsPaper.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/7a-EquationsPaper-SpMort-Phe-tempOFF
