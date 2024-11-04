6af331bb48f269aec58b95d2fb2488856fabe181
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-11-04 09:05:42 -0800

    reduce FHE high uptake to 0.001 for all contams, adjust ReprodEffect parameters

--- Run_highres.sh
+++ Run_highres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_year1_highres_EqnsPaper.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/7a-EquationsPaper-90d-lowFHEuptake
+atlantis run ./atlantis_year1_highres_EqnsPaper.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/7a-EquationsPaper-90d-highFHEuptake2
