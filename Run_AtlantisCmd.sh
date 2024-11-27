a6a368df241e0ad8892288cf2f7ba3b2ea31eae7
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2024-11-27 15:52:26 -0800

    v6718 oil spill 50y run with old bio file + updated params, Box-60 closure for 30 days

--- Run_AtlantisCmd.sh
+++ Run_AtlantisCmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_winter.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/v6718_Scenario_5b_2019-01-20-cBox60-for30d
+atlantis run ./atlantis_winter.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/v6718_Scenario_5b_2019-01-20-cBox60-for30d-2
