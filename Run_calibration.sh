a77507e891656d54f4af2d321aea4056fd42435b
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2025-04-24 17:38:20 -0700

    test reducing k_DL light absorption of labile detritus

--- Run_calibration.sh
+++ Run_calibration.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_calibration_RL.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/v6719_50y_RL_newmig-mixdeep-25-2
+atlantis run ./atlantis_calibration.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/v6719_50y_VBITBUCKET-PL08-kDL
