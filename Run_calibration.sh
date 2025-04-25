29b94b5a5b19fe09ca6ad3800c82f94e8a34467b
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2025-04-25 12:30:30 -0700

    combined adjustment PL06 PS06 kDL mumMA

--- Run_calibration.sh
+++ Run_calibration.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_calibration.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/v6719_50y_VBITBUCKET-PL08-kDL
+atlantis run ./atlantis_calibration.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/v6719_50y_VBITBUCKET_PL09-PS06-kDL-mMA
