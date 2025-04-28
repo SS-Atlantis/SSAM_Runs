5365b12b01e00c44e54712ff159189509e4f987d
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2025-04-28 07:34:05 -0700

    deccrease ZS_mL in best run

--- Run_calibration.sh
+++ Run_calibration.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_calibration.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/v6719_50y_VBITBUCKET_PL09-PS06-kDL-mMA
+atlantis run ./atlantis_calibration.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/v6719_50y_VBITBUCKET_PL08-PS06-ZSmL03
