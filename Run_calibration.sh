4f09085131692f935070040a3f2618de3a8c77ac
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2025-05-04 16:45:46 -0700

    update for dual test runs

--- Run_calibration.sh
+++ Run_calibration.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_calibration.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/v6719_50y_VBITBUCKET_PL08-PS06-ZSmL03
+atlantis run ./atlantis_calibration_RL.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/v6719_50y_RL_PL09-mLZ
