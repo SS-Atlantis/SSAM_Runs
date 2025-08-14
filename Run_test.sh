7dc38cdca11b6ec1d0ff92dd02669e17b1cb5cf8
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2025-08-13 18:04:46 -0700

    tests for contaminants in epifauna

--- Run_test.sh
+++ Run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_calibration_RL.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/v6719_50y_VBITBUCKET-ddmove0-2-ZGppreyWS-1-PL-fishing
+atlantis run ./atlantis_highres_d0.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Tests/epifauna
