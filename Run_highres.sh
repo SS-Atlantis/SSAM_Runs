8b021dee2d90089093821dde3716cb50a576209a
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2025-11-21 14:35:06 -0800

    3-yr controls for highres output

--- Run_highres.sh
+++ Run_highres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/MPRI-1/highres-scenario_5b_2019-01-20
+atlantis run ./atlantis_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/MPRI-1/highres-control-3y-2039-2042
