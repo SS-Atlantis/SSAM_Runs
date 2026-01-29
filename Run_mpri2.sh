77116e6446a091200110bcf22bed9d3e6371e910
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2026-01-29 13:12:27 -0800

    run fish closure control and 5b spill test

--- Run_mpri2.sh
+++ Run_mpri2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_winter.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/Control/highres-control-3y-2019-2022_5b2019FC90
+atlantis run ./atlantis_winter.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/MPRI2/highres-2019-2022_5b_1_2019-01-20_FC90
