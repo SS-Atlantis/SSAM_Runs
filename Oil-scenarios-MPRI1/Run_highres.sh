b3dc921ad20f4677b511475f6ce04b1060a6e442
Raisha Lovindeer <rlovindeer@eoas.ubc.ca>
2026-04-19 15:47:15 -0700

    update all runs with water soluble fraction pseudo-components of PAH

--- Oil-scenarios-MPRI1/Run_highres.sh
+++ Oil-scenarios-MPRI1/Run_highres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-atlantis run ./atlantis_highres.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/MPRI1/highres-2019-2022_7a_4_2020-01-03
+atlantis run ./atlantis_winter_release.yaml /ocean/rlovindeer/MOAD/analysis-raisha/SSmodel_outputs/Spills/MPRI1/highres-2029-2039_5b_4_2020-01-16
